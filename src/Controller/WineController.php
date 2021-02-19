8b7225c27f4fd34dd433242d61c3a5277f0129c1
Nathan Gonzalez <n.gonzalez@cavavin.fr>
2021-02-19 09:39:24 +0100

    first commit

---
+++ src/Controller/WineController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WineController extends AbstractController
+{
+    /**
+     * @Route("/wine", name="wine")
+     */
+    public function index(): Response
+    {
+        return $this->render('wine/index.html.twig', [
+            'controller_name' => 'WineController',
+        ]);
+    }
+}
---
+++ src/Entity/Wine.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WineRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WineRepository::class)
+ */
+class Wine
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $millesime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMillesime(): ?string
+    {
+        return $this->millesime;
+    }
+
+    public function setMillesime(string $millesime): self
+    {
+        $this->millesime = $millesime;
+
+        return $this;
+    }
+}
